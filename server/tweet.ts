5646e1a8230029f53505898e646d8fed00fbb45a
Andrew Lisowski <lisowski54@gmail.com>
2019-02-25 11:32:33 -0800

    make local dev good again

--- server/tweet.ts
+++ server/tweet.ts
@@ -49,10 +49,41 @@ function setUpTwitter(token: string) {
   };
 }
 
-app.get('*', async (req, res) => {
-  try {
+async function getGuestToken() {
+  if (process.env.NODE_ENV === 'production') {
     const tokenResponse = await fetch('http://76.167.236.46:3000/api/token');
     const token = await tokenResponse.text();
+
+    return token;
+  }
+
+  const result = await fetch(
+    'https://mobile.twitter.com/ericclemmons/status/1098673740156530688',
+    {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.81 Safari/537.36'
+      }
+    }
+  );
+  const body = await result.text();
+  const match = body.match(/gt=(\d+)/);
+
+  if (!match) {
+    throw new Error('No token found!');
+  }
+
+  return match[1];
+}
+
+app.get('*', async (req, res) => {
+  if (!req.query.tweet) {
+    res.send('No tweet id provided! Call this endpoint like: /?tweet=TWEET_ID');
+    return;
+  }
+
+  try {
+    const token = await getGuestToken();
     const twitter = setUpTwitter(token);
     const json = await twitter.get(req.query.tweet);
     const treeConverter = new TreeConverter(json, req.query.tweet, twitter.get);
@@ -63,5 +94,17 @@ app.get('*', async (req, res) => {
     console.log(error);
   }
 });
+
+const PORT = process.env.PORT || 3001;
+
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, (err: Error) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log(`> Ready on ${PORT}`);
+  });
+}
 
 export default app;
