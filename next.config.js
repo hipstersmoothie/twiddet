da07782caba8fe91c4101c2d9beff7b565427912
Andrew Lisowski <lisowski54@gmail.com>
2019-02-25 17:15:55 -0800

    switch now builders for better release (should work for PR releases too)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withTypescript = require('@zeit/next-typescript');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = withTypescript({
+  target: 'serverless',
   webpack(config, options) {
     // Do not run type checking twice:
     if (options.isServer) {
